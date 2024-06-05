1b0c5b69c04659451c3c399f6f985ae7adb06a03
Tatiana8972 <tatvorontsova02@mail.ru>
2024-06-05 11:11:49 +0500

    Исправляет

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -4,7 +4,7 @@ const navToggle = document.querySelector('.nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
 
-navToggle.addEventListener('click', function () {
+navToggle.addEventListener('click', () => {
   if (navMain.classList.contains('nav--closed')) {
     navMain.classList.remove('nav--closed');
     navMain.classList.add('nav--opened');
